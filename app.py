1a3f6cfd76361a958245b1d46f1e8774d3418507
ViniciusMartins <vrmartins05@gmail.com>
2020-03-10 11:48:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Initialize Logger
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
+
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """"Reverse and return the provides URI"""
+    LOGGER.info('Received a message: %s', random_string)
     return "".join(reversed(random_string))
 
 if __name__ == '__main__':
